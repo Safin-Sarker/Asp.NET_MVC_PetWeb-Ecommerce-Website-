571ff6cbcec752458b6f1450f19c3f738d7d07a1
Safin-Sarker <safinsarker1122@gmail.com>
2024-06-19 19:15:59 +0200

    N Tier Architecture

---
+++ src code/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PetWeb.Models;
+
+namespace PetWeb.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        
+    }
+}
