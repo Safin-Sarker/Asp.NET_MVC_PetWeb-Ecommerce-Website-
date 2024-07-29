c58ee6f7bacabf5e87c57cd172f21ea84c96c161
Safin-Sarker <safinsarker1122@gmail.com>
2024-07-29 20:17:12 +0200

    Create More project for N-tier Architecture Setup

---
+++ Asp.NET_MVC_PetWeb-Ecommerce-Website-/PetWeb.DataAccess/Data/ApplicationDbContext1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetWeb.Models;
+
+namespace PetWeb.DataAccess.Data
+{
+    public class ApplicationDbContext1:DbContext
+    {
+        public ApplicationDbContext1(DbContextOptions<ApplicationDbContext1> options)
+           : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id=1, Name="Action", DisplayOrder=1},
+                new Category { Id = 2, Name = "Scify", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+            
+        }
+    }
+}
